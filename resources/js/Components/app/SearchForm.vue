e12771d772b77c5428d4a62aec00be0bc82c9ca8
LordWatson <alexander.watson.work@gmail.com>
2024-03-31 14:20:18 +0100

    created some vue components

---
+++ resources/js/Components/app/SearchForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Search Form
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SearchForm"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/app/UserSettingsDropdown.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        User Settings Dropdown
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UserSettingsDropdown"
+}
+</script>
+
+<style scoped>
+
+</style>
